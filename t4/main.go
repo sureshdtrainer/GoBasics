8ab7f7b09ee8302e4d738254a439352141adfc63
suresdtrainer <suresh.d.trainer@gmail.com>
2025-01-23 13:50:50 +0530

    t6  with conditional and loops

--- t4/main.go
+++ t4/main.go
@@ -9,10 +9,36 @@ func main() {
 	var printValue = "Printing Hello World"
 	printMe(printValue)
 
-	var numerator = 21
-	var denominator = 2
+	var numerator = 20
+	var denominator = 7
 	var result, reminder, err = divide(numerator, denominator)
-	fmt.Println(result, reminder, err)
+	//fmt.Println(result, reminder, err)
+	//if else
+	if err != nil {
+		fmt.Println(err.Error())
+	} else if reminder == 0 {
+		fmt.Printf("\nThe result of division is %v\n", result)
+	} else {
+		fmt.Printf("\nThe result of the division is %v with reminder %v", result, reminder)
+	}
+	//switch
+	switch {
+	case err != nil:
+		fmt.Println(err.Error())
+	case reminder == 0:
+		fmt.Printf("\nThe result of division is %v\n", result)
+	default:
+		fmt.Printf("\nThe result of the division is %v with reminder %v", result, reminder)
+	}
+	switch reminder {
+	case 0:
+		fmt.Println("\nThe division was exact")
+	case 1, 2, 3:
+		fmt.Println("\nThe division was close")
+	default:
+		fmt.Println("\nThe divsion was NOT close")
+	}
+
 }
 
 func printMe(printValue string) {
